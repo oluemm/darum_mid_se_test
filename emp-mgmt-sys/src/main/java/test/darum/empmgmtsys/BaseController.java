b4cced68857690c3ca71074953e9d7a754645e5a
Oluwafemi Emmanuel <fasogbaemmanuel@gmail.com>
2025-11-10 06:42:51 +0100

    Add auto-redirect to swagger docs
    Signed-off-by: Oluwafemi Emmanuel <fasogbaemmanuel@gmail.com>

--- emp-mgmt-sys/src/main/java/test/darum/empmgmtsys/BaseController.java
+++ emp-mgmt-sys/src/main/java/test/darum/empmgmtsys/BaseController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RefreshScope
@@ -11,8 +12,14 @@ public class BaseController {
   @Value("${app.name}")
   private String appName;
 
-  @GetMapping("get-config")
+  @GetMapping("configs")
   public String getAppName() {
     return "Application name: " + appName;
+  }
+
+  @GetMapping("/")
+  public RedirectView index() {
+
+    return new RedirectView("/swagger-ui/index.html");
   }
 }
