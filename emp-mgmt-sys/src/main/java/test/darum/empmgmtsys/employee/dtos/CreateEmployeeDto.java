e6e6366d9d7d558f7965e02934e5c7773892f4ec
Oluwafemi Emmanuel <fasogbaemmanuel@gmail.com>
2025-11-10 08:17:50 +0100

    Add validation rules
    Signed-off-by: Oluwafemi Emmanuel <fasogbaemmanuel@gmail.com>

--- emp-mgmt-sys/src/main/java/test/darum/empmgmtsys/employee/dtos/CreateEmployeeDto.java
+++ emp-mgmt-sys/src/main/java/test/darum/empmgmtsys/employee/dtos/CreateEmployeeDto.java
@@ -1,6 +1,7 @@
 package test.darum.empmgmtsys.employee.dtos;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,11 +11,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class CreateEmployeeDto {
-  @NotNull
+
+  @NotNull(message = "First name is required")
   private String firstName;
   private String lastName;
-  @NotNull
+
+  @Email(message = "Provide a valid email address")
   private String email;
-  @NotNull
+  
+  @NotNull(message = "User's status is required")
   private String status;
 }
